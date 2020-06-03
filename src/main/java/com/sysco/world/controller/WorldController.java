1be7dc33d4de0f7df07fc2be32b9a44e64e7e9bc
cpage-pivotal <cpage@pivotal.io>
2020-06-03 11:56:53 -0500

    Change Color to gray

--- src/main/java/com/sysco/world/controller/WorldController.java
+++ src/main/java/com/sysco/world/controller/WorldController.java
@@ -2,7 +2,6 @@ package com.sysco.world.controller;
 
 import com.sysco.world.config.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
